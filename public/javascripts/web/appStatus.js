17845f344534311ab8bef7359ef0f211cd22b182
David Monllao <davidm@moodle.com>
2014-06-20 23:31:08 +0700

    [FRONTEND] Website-specific JS

---
+++ public/javascripts/web/appStatus.js
@@ -0,0 +1,26 @@
+$.appStatus = {
+
+    waiting : function() {
+
+        if (!$('#id-mask').hasClass('modal')) {
+            $('#id-mask').addClass('modal');
+        }
+
+        // Disables action buttons.
+        $('.btn').removeClass('btn-primary');
+        $('.btn').addClass('btn-disabled');
+        $('.btn').addClass('btn-disabled');
+    },
+
+    completed : function() {
+
+        if ($('#id-mask').hasClass('modal')) {
+            $('#id-mask').removeClass('modal');
+        }
+
+         // Enable buttons again.
+        $('.btn').removeClass('btn-default');
+        $('.btn').addClass('btn-primary');
+        $('.btn').removeClass('btn-disabled');
+    }
+};
---
+++ public/javascripts/web/notifier.js
@@ -0,0 +1,33 @@
+$.notifier = {
+
+    show : function(msg, type) {
+
+        var className = '';
+        if (type === 'success') {
+            className = 'alert-success';
+        } else if (type === 'info') {
+            className = 'alert-info';
+        } else if (type === 'error') {
+            className = 'alert-danger';
+        } else {
+            console.log('Not supported notification type: ' + type, error);
+            return;
+        }
+
+        // Smashes the current value.
+        $('#id-notification').html(msg);
+        $('#id-notification').addClass(className);
+
+        $('#id-notification').fadeIn(2000);
+
+        // We hide it in a few secs.
+        setTimeout(function() {
+            $('#id-notification').fadeOut(2000);
+
+            // We need to wait until it completely fades out.
+            setTimeout(function() {
+                $('#id-notification').removeClass(className);
+            }, 2000);
+        }, 3000);
+    },
+};
