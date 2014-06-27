3dbe7bb2aaf7eb2f33a6da50ea56de07476f52af
David Monllao <david.monllao@gmail.com>
2014-06-27 08:49:38 +0800

    [FRONTEND] Move datetime visibility to datetime scope

--- public/javascripts/controllers/index.js
+++ public/javascripts/controllers/index.js
@@ -17,22 +17,6 @@ angular.module('eat-this-one').controller('indexController', ['$scope', '$window
         value: ''
     };
 
-    // date/time pickers hidden by default.
-    $scope.showDatepicker = false;
-    $scope.showTimepicker = false;
-
-    // Toggles the datepicker.
-    $scope.toggleDatepicker = function($event) {
-        $scope.showTimepicker = false;
-        $scope.showDatepicker = $scope.showDatepicker == false ? true : false;
-    };
-
-    // Toggles the timepicker
-    $scope.toggleTimepicker = function($event) {
-        $scope.showDatepicker = false;
-        $scope.showTimepicker = $scope.showTimepicker == false ? true : false;
-    };
-
     // Redirects to search dish page.
     $scope.searchDish = function() {
         $window.location.href = '/dishes/index.html' + $scope.getParams();
--- public/javascripts/directives/datetime.js
+++ public/javascripts/directives/datetime.js
@@ -2,9 +2,26 @@ angular.module('eat-this-one').directive('eatDatetime', function() {
 
     return {
         restrict: 'E',
-        transclude: true,
         scope: {
-            element: '='
+            element: '=',
+        },
+        link : function(scope) {
+
+            // date/time pickers hidden by default.
+            scope.showDatepicker = false;
+            scope.showTimepicker = false;
+
+            // Toggles the datepicker.
+            scope.toggleDatepicker = function($event) {
+                scope.showTimepicker = false;
+                scope.showDatepicker = scope.showDatepicker == false ? true : false;
+            };
+
+            // Toggles the timepicker
+            scope.toggleTimepicker = function($event) {
+                scope.showDatepicker = false;
+                scope.showTimepicker = scope.showTimepicker == false ? true : false;
+            };
         },
         templateUrl: "templates/datetime.html"
     };
