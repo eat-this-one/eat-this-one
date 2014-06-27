4d249f9300935506215dbb62d9cfddb5d9d22ec2
David Monllao <davidm@moodle.com>
2014-06-27 17:53:26 +0800

    ASD

--- test/controllers/index.js
+++ test/controllers/index.js
@@ -13,4 +13,18 @@ describe('Index Controller', function() {
     it('should contain a page title', function() {
         expect(scope.pageTitle).toBe('Eat-this-one');
     });
+
+    it('should default to empty fields', function() {
+        expect(scope.where.value.toBe('');
+        expect(scope.when.value.toBe('');
+    });
+
+    it('should default to hidden timepickers and datepickers', function() {
+    });
+
+    it('should show the calendar when clicking the calendar button', function() {
+    });
+
+    it('should show the calendar when clicking the calendar button', function() {
+    });
 });
