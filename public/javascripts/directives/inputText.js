8b1564b60ad82f25cd7da98b931105cb015a2729
David Monllao <david.monllao@gmail.com>
2014-06-28 01:53:10 +0800

    [FRONTEND] Renaming eat-field to eat-input-text

---
+++ public/javascripts/directives/inputText.js
@@ -0,0 +1,11 @@
+angular.module('eat-this-one').directive('eatInputText', function() {
+
+    return {
+        restrict: 'E',
+        scope: {
+            element: '=',
+        },
+        templateUrl: "/templates/input-text.html"
+    };
+
+});
