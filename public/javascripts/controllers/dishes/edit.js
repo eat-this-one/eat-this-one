f0110aca9664922a1060c217a8d3a9f43b20cb5c
David Monllao <david.monllao@gmail.com>
2014-06-27 08:55:32 +0800

    [FRONTEND] Dish edit page using directives (wrongly now)

--- public/javascripts/controllers/dishes/edit.js
+++ public/javascripts/controllers/dishes/edit.js
@@ -6,18 +6,69 @@ angular.module('eat-this-one')
 
     $scope.dish = {};
 
+    // Arguments.
     var id = urlParser.getParam('id');
 
+    var where = urlParser.getParam('where');
+    if (where) {
+        $scope.dish.where = where;
+    }
+
+    // We push the value as from.
+    var when = urlParser.getParam('when');
+    if (when) {
+        $scope.dish.from = when;
+    }
+
+    // Form fields.
+    $scope.name = {
+        name: 'name',
+        label: $scope.lang.dishname,
+        placeholder: $scope.lang.dishname,
+        value: ''
+    };
+    $scope.description = {
+        name: 'description',
+        label: $scope.lang.dishdescription,
+        placeholder: $scope.lang.dishdescription,
+        value: ''
+    };
+    $scope.user = {
+        name: 'user',
+        label: $scope.lang.username,
+        placeholder: $scope.lang.username,
+        value: ''
+    };
+    $scope.where = {
+        name: 'where',
+        label: $scope.lang.where,
+        placeholder: $scope.lang.where,
+        value: ''
+    };
+    $scope.from = {
+        name: 'from',
+        label: $scope.lang.from,
+        placeholder: $scope.lang.from,
+        value: ''
+    };
+    $scope.to = {
+        name: 'to',
+        label: $scope.lang.to,
+        placeholder: $scope.lang.to,
+        value: ''
+    };
+
     // Load the dish info.
-    appStatus.waiting();
-    dishRequest($scope, id);
+    if (id) {
+        // TODO: Check that the user is the owner
+        appStatus.waiting();
+        dishRequest($scope, id);
+    }
 
     $scope.save = function() {
 
         // TODO Probably some dish cleaning will be required.
         var dish = $scope.dish;
-        dish.from = new Date(2014, 06, 06, 14, 30, 00);
-        dish.to = new Date(2014, 06, 06, 15, 30, 00);
 
         appStatus.waiting();
         editDishRequest($scope, dish);
