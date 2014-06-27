76b70161f7226d36690e3900dcdddef9c4734da0
David Monllao <david.monllao@gmail.com>
2014-06-28 01:55:40 +0800

    [FRONTEND] Error shouldn't disappear

--- public/javascripts/web-services/notifier.js
+++ public/javascripts/web-services/notifier.js
@@ -22,14 +22,16 @@ angular.module('eat-this-one').factory('notifier', function() {
             $('#id-notification').fadeIn(2000);
 
             // We hide it in a few secs.
-            setTimeout(function() {
-                $('#id-notification').fadeOut(2000);
-
-                // We need to wait until it completely fades out.
+            if (type !== 'error') {
                 setTimeout(function() {
-                    $('#id-notification').removeClass(className);
-                }, 2000);
-            }, 3000);
+                    $('#id-notification').fadeOut(2000);
+
+                    // We need to wait until it completely fades out.
+                    setTimeout(function() {
+                        $('#id-notification').removeClass(className);
+                    }, 2000);
+                }, 3000);
+            }
         }
 
     }
