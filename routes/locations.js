a123a4dfe3192893d05896f6ba35f97ead7ca4a7
David Monllao <david.monllao@gmail.com>
2014-07-10 08:22:00 +0800

    [BACKEND] Adding locations/ routes

--- routes/locations.js
+++ routes/locations.js
@@ -9,7 +9,26 @@ var LocationModel = require('../models/location.js').model;
 // GET - Locations list.
 router.get('/', function(req, res) {
 
-    LocationModel.find(function(error, locations) {
+    // Here we accept filters
+    var filter = {};
+    if (req.param('name') || req.param('address')) {
+        if (req.param('name')) {
+            if (req.param('regex')) {
+                filter.name = new RegExp(req.param('name'), 'i');
+            } else {
+                filter.name = req.param('name');
+            }
+        }
+        if (req.param('address')) {
+            if (req.param('regex')) {
+                filter.address = new RegExp(req.param('address'), 'i');
+            } else {
+                filter.address = req.param('address');
+            }
+        }
+    }
+
+    LocationModel.find(filter, function(error, locations) {
         if (error) {
             console.log(error);
             res.send("No locations." + error);
@@ -24,23 +43,22 @@ router.get('/:id', function(req, res) {
 
     var id = req.param('id');
 
-    LocationModel.findById(id, function(error, location) {
+    LocationModel.findById(id, function(error, loc) {
         if (error) {
             console.log(error);
             res.send("Location '" + id + "' not found. " + error);
         }
         res.statusCode = 200;
-        res.send(location);
+        res.send(loc);
     });
 });
 
 // POST - Create a location.
 router.post('/', function(req, res) {
+
     var locationProps = {
         'name' : 'required',
-        'shortname' : 'required',
-        'latLng' : 'required',
-        'userid' : 'required'
+        'address' : 'required'
     };
 
     var locationObj = {};
@@ -58,19 +76,41 @@ router.post('/', function(req, res) {
         res.statusCode = 400;
         res.send("Missing params, can not create location");
     }
-    var location = new LocationModel(locationObj);
 
-    location.save(function(error) {
+    TokenModel.findOne({token: req.param('token')}, function(error, token) {
+
         if (error) {
-            console.log(error);
-            res.statusCode = 400;
-            res.send("Can not save location " + req.param('name'));
+            res.statusCode = 401;
+            res.send('Wrong credentials');
         }
-    });
 
-    // Same output for all output formats.
-    res.statusCode = 201;
-    res.send(location);
+        if (token === null) {
+            res.statuscode = 401;
+            res.send('Wrong credentials');
+        }
+
+        // Setting the userid from the token.
+        locationObj.userid = token.userid;
+
+        var locationInstance = new LocationModel(locationObj);
+        locationInstance.save(function(error) {
+            if (error) {
+                console.log(error);
+                res.statusCode = 400;
+                res.send("Can not save location " + req.param('name'));
+            }
+        });
+
+        // Now we auto-subscribe the user to that location.
+        var locationSubscriptionInstance = new LocationSubscriptionModel({
+            userid: token.userid,
+            locationid: locationInstance.id
+        });
+
+        // Same output for all output formats.
+        res.statusCode = 201;
+        res.send(locationInstance);
+    });
 });
 
 router.put('/:id', function(req, res) {
