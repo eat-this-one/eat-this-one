cd805af032717ca1781f3520e82a055d3f00525c
David Monllao <david.monllao@gmail.com>
2014-07-13 09:34:20 +0800

    [FRONTEND] Errors also fade out, like the failed login one

--- public/javascripts/web-services/notifier.js
+++ public/javascripts/web-services/notifier.js
@@ -22,16 +22,14 @@ angular.module('eat-this-one').factory('notifier', function() {
             $('#id-notification').fadeIn(2000);
 
             // We hide it in a few secs.
-            if (type !== 'error') {
-                setTimeout(function() {
-                    $('#id-notification').fadeOut(2000);
+            setTimeout(function() {
+                $('#id-notification').fadeOut(2000);
 
-                    // We need to wait until it completely fades out.
-                    setTimeout(function() {
-                        $('#id-notification').removeClass(className);
-                    }, 2000);
-                }, 3000);
-            }
+                // We need to wait until it completely fades out.
+                setTimeout(function() {
+                    $('#id-notification').removeClass(className);
+                }, 2000);
+            }, 3000);
         }
 
     }
