3c19dd8b2d37dc09f481650367cef9999cb0e831
David Monllao <davidm@moodle.com>
2014-08-11 10:07:06 +0200

    [FRONTEND] Messages handler

---
+++ public/javascripts/shared-services/messagesHandler.js
@@ -0,0 +1,9 @@
+angular.module('eat-this-one')
+    .factory('messagesHandler', ['notifier', function(notifier) {
+
+    return {
+        message : function(msgData) {
+            notifier.statusBar('Eat this one', msgData.message);
+        }
+    }
+}]);
