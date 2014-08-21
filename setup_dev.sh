531791def1f10598769f19a95392c7d77fc59c42
David Monllao <davidm@moodle.com>
2014-08-22 00:32:49 +0200

    [FRONTEND] New plugin

--- setup_dev.sh
+++ setup_dev.sh
@@ -19,3 +19,4 @@ cordova plugin add org.apache.cordova.device
 cordova plugin add https://github.com/phonegap-build/PushPlugin.git
 cordova plugin add https://github.com/katzer/cordova-plugin-local-notifications.git
 cordova plugin add org.apache.cordova.camera
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-dialogs.git
