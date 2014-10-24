2877f8141c6405f84b63b7f6f903a107d237fe95
David Monllao <davidm@moodle.com>
2014-10-24 13:06:46 +0800

    [FRONTEND] New page for meals

---
+++ public/javascripts/controllers/meals/view.js
@@ -0,0 +1,35 @@
+angular.module('eat-this-one')
+    .controller('MealsViewController', ['$scope', 'appStatus', 'urlParser', 'dishRequest', 'eatConfig', 'authManager', '$modal', function($scope, appStatus, urlParser, dishRequest, eatConfig, authManager, $modal) {
+
+    $scope.lang = $.eatLang.lang;
+    $scope.auth = authManager;
+
+    // Page title.
+    $scope.pageTitle = $scope.lang.dish;
+
+    $scope.loginButtonsLangStrings = {
+        signin : $scope.lang.signintoorder,
+        signup : $scope.lang.signuptoorder
+    };
+
+    $scope.dish = {};
+
+    $scope.openStaticMap = function() {
+        var staticMapModal = $modal.open({
+            templateUrl: 'templates/static-map.html',
+            controller: 'staticMapController',
+            resolve: {
+                dish : function() {
+                    return $scope.dish;
+                }
+            }
+        });
+    };
+
+    var id = urlParser.getParam('id');
+
+    // Load the dish info into the fields.
+    appStatus.waiting();
+    dishRequest($scope, id);
+
+}]);
