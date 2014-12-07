5ab59f46d734cd0aea6ec1abbfc58ca6190eea90
David Monllao <david.monllao@gmail.com>
2014-12-08 02:50:02 +0800

    [BACKEND] Store the download file outside the repo

--- routes/download.js
+++ routes/download.js
@@ -1,20 +1,10 @@
 var express = require('express');
-var path = require('path');
-var fs = require('fs');
+var nconf = require('nconf');
 
 var router = express.Router();
 
 router.get('/eat-this-one.apk', function(req, res) {
-    var filePath = path.join(__dirname, '/../download/' + 'eat-this-one.apk');
-    if (fs.existsSync(filePath)) {
-        res.statusCode = 200;
-        res.download(filePath);
-        return;
-    } else {
-        res.statusCode = 500;
-        res.send('Error: File does not exist');
-        return;
-    }
+    res.redirect(nconf.get('ANDROID_DOWNLOAD_URL'));
 });
 
 module.exports = router;
