cf72110b06680f8b9567c036cfcd5b8edd336f8b
David Monllao <david.monllao@gmail.com>
2014-12-11 16:26:06 +0800

    [FRONTEND] Fixing megacagada cagadissima

--- public/javascripts/controllers/staticMap.js
+++ public/javascripts/controllers/staticMap.js
@@ -2,6 +2,7 @@ angular.module('eat-this-one')
     .controller('staticMapController', ['$scope', 'eatConfig', '$modalInstance', 'newLogRequest', function($scope, eatConfig, $modalInstance, newLogRequest) {
 
         $scope.lang = $.eatLang.lang;
+        $scope.dish = dish;
 
         $scope.close = function() {
             $modalInstance.close();
