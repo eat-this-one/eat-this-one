1bb5239df708f6b580cf1b831f4710aa9fbe7a07
David Monllao <david.monllao@gmail.com>
2014-12-12 08:43:20 +0800

    [FRONTEND] Fixing notification waits

--- public/javascripts/app-services/notifier.js
+++ public/javascripts/app-services/notifier.js
@@ -18,20 +18,22 @@ angular.module('eat-this-one').factory('notifier', function() {
 
             var msgid = Math.random() * 1000;
 
-            window.plugin.notification.local.add({
-                id : msgid,
-                title : title,
-                message : message,
-                json : { dishid : dishid, type: type},
-            });
+            document.addEventListener('deviceready', function() {
+                window.plugin.notification.local.add({
+                    id : msgid,
+                    title : title,
+                    message : message,
+                    json : { dishid : dishid, type: type},
+                });
 
-            // TODO Not always working.
-            // Move to the requested page.
-            window.plugin.notification.local.onclick = function(id, state, json) {
-                var href = 'dishes/view.html?id=' +JSON.parse(json).dishid;
-                console.log('Notification clicked, user redirected to ' + href);
-                window.location.href = href;
-            };
+                // TODO Not always working.
+                // Move to the requested page.
+                window.plugin.notification.local.onclick = function(id, state, json) {
+                    var href = 'dishes/view.html?id=' +JSON.parse(json).dishid;
+                    console.log('Notification clicked, user redirected to ' + href);
+                    window.location.href = href;
+                };
+            });
         }
     }
 });
