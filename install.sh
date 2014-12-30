c303b33e762b98933ae8747d12a3501efde902d8
David Monllao <davidm@moodle.com>
2014-12-30 20:02:05 +0800

    [PROJECT] New update.sh script to update dependencies

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Install global dependencies.
+sudo npm install grunt-cli -g
+sudo npm install express -g
+sudo npm install bower -g
+sudo npm install express-generator -g
+sudo npm install cordova -g
+
+# Install project dependencies.
+sudo npm install
+bower install
+
+# Create cordova app and add dependencies.
+cordova create dist/app "com.monllao.david.eatthisone" "Eat this one" --copy-from=public/shared-build
+
+# App icon.
+ln icon.png dist/app/icon.png
+
+cd dist/app
+
+# TODO: Other platforms.
+cordova platform add android
+
+# Install all plugins.
+while read -a plugin; do
+    cordova plugin add ${plugin[1]}
+done < ../../cordova-plugins.list
---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Update global dependencies.
+sudo npm update grunt-cli -g
+sudo npm update express -g
+sudo npm update bower -g
+sudo npm update express-generator -g
+sudo npm update cordova -g
+
+# Update project dependencies.
+sudo npm update
+bower update
+
+cd dist/app
+
+# TODO: Other platforms.
+cordova platform update android
+
+# Update all plugins.
+while read -a plugin; do
+    cordova plugin rm ${plugin[0]}
+    cordova plugin add ${plugin[1]}
+done < ../../cordova-plugins.list
