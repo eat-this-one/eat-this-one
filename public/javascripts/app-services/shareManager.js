bb78073fe9407a89bd5212c4072a4feaf0aaf864
David Monllao <davidm@moodle.com>
2015-01-04 23:01:31 +0800

    [FRONTEND] Externalizes share manager
    To allow web-services to be created.

---
+++ public/javascripts/app-services/shareManager.js
@@ -0,0 +1,114 @@
+angular.module('eat-this-one').factory('shareManager', ['appStatus', 'notifier', 'newLogRequest', function(appStatus, notifier, newLogRequest) {
+
+    return {
+
+        init : function($scope) {
+
+            document.addEventListener('deviceready', function() {
+
+                // Set the loading here as getting contacts may be slow.
+                appStatus.waiting('contacts');
+
+                function onSuccess(contacts) {
+
+                    // Expensive method.
+                    for (var i = 0; i < contacts.length; i++) {
+                        if (contacts[i].displayName === "" && contacts[i].displayName == null) {
+                            break;
+                        }
+                        if (typeof contacts[i].phoneNumbers != 'undefined' && contacts[i].phoneNumbers != null) {
+                            for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+                                if (contacts[i].phoneNumbers[j].type === 'mobile') {
+                                    // We pick the first one removing spaces from the phone number.
+                                    // TODO Avoid duplicates adding a contacts[mobilePhone] key.
+                                    $scope.contacts.push({
+                                        displayName: contacts[i].displayName,
+                                        mobilePhone: contacts[i].phoneNumbers[j].value.replace(/\s+/g, '')
+                                    });
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    // Update $scope.contacts.
+                    $scope.$apply();
+
+                    // Set a class once a contact is selected.
+                    $('#id-contacts .contact').each(function() {
+                        $(this).on('click', function(e) {
+                            // Toggles active class.
+                            if ($(this).hasClass('active')) {
+                                $(this).removeClass('active');
+                                $(this).children('span.glyphicon').removeClass('glyphicon-ok')
+                            } else {
+                                $(this).addClass('active');
+                                $(this).children('span.glyphicon').addClass('glyphicon-ok')
+                            }
+                        });
+                    });
+
+                    // All done.
+                    appStatus.completed('contacts');
+                };
+
+                function onError(contactError) {
+                    notifier.show($scope.lang.error, $scope.lang.errornocontacts, 'error');
+                    console.log('Error getting contacts: ' + contactError);
+
+                    appStatus.completed('contacts');
+                };
+
+                var filter = [
+                    navigator.contacts.fieldType.displayName,
+                    navigator.contacts.fieldType.name
+                ];
+                // TODO Try to filter by something (not null...) to avoid the big big iteration.
+                var options = new ContactFindOptions();
+                options.filter = "";
+                options.multiple = true;
+                options.desiredFields = [
+                    navigator.contacts.fieldType.displayName,
+                    navigator.contacts.fieldType.name,
+                    navigator.contacts.fieldType.phoneNumbers
+                ];
+                navigator.contacts.find(filter, onSuccess, onError, options);
+
+            }, false);
+
+        },
+
+        process : function($scope) {
+
+            appStatus.waiting('selectedContacts');
+
+            // Get all selected contacts.
+            var phonesArray = [];
+            $('#id-contacts .contact.active').each(function() {
+                console.log('contact! ' + this.id);
+                phonesArray.push(this.id);
+            });
+
+            // The user should explicitly press 'Skip'.
+            if (phonesArray.length === 0) {
+                notifier.show($scope.lang.nocontacts, $scope.lang.nocontactsinfo, 'warning');
+                appStatus.completed('selectedContacts');
+                return false;
+            }
+
+            // The receiver already knows who is sending the message.
+            var msg = $scope.lang.imcooking + ' ' + urlParser.getParam('dishname') + '. ' +
+                 $scope.lang.joindetailsbook + '. ' + $scope.lang.groupcode +
+                 ': "' + urlParser.getParam('locationname') + '". ' + eatConfig.downloadAppUrl;
+
+            appStatus.completed('selectedContacts');
+
+            var phonesStr = phonesArray.join(',');
+
+            newLogRequest('click', 'share-contacts', phonesStr);
+
+            // Open sms app.
+            window.plugins.socialsharing.shareViaSMS(msg, phonesStr);
+        }
+    };
+}]);
--- public/javascripts/controllers/dishes/share.js
+++ public/javascripts/controllers/dishes/share.js
@@ -1,89 +1,20 @@
 angular.module('eat-this-one')
-    .controller('DishesShareController', ['$scope', 'redirecter', 'appStatus', 'eatConfig', 'urlParser', 'notifier', 'newLogRequest', function($scope, redirecter, appStatus, eatConfig, urlParser, notifier, newLogRequest) {
+    .controller('DishesShareController', ['$scope', 'redirecter', 'eatConfig', 'urlParser', 'shareManager', 'newLogRequest', function($scope, redirecter, eatConfig, urlParser, shareManager, newLogRequest) {
 
     $scope.lang = $.eatLang.lang;
 
     // Page title.
     $scope.pageTitle = $scope.lang.addcolleagues;
 
-    // To store the user contacts with a valid name and mobile phone.
+    // Allow shareManager services to inject a contacts list here.
     $scope.contacts = [];
-
-    // Set the loading here as getting contacts may be slow.
-    appStatus.waiting('contacts');
 
     newLogRequest('view', 'dishes-share');
 
-    document.addEventListener('deviceready', function() {
+    // Initializes the share manager (loads phone contacts if necessary...).
+    shareManager.init($scope);
 
-        function onSuccess(contacts) {
-
-            // Expensive method.
-            for (var i = 0; i < contacts.length; i++) {
-                if (contacts[i].displayName === "" && contacts[i].displayName == null) {
-                    break;
-                }
-                if (typeof contacts[i].phoneNumbers != 'undefined' && contacts[i].phoneNumbers != null) {
-                    for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-                        if (contacts[i].phoneNumbers[j].type === 'mobile') {
-                            // We pick the first one removing spaces from the phone number.
-                            // TODO Avoid duplicates adding a contacts[mobilePhone] key.
-                            $scope.contacts.push({
-                                displayName: contacts[i].displayName,
-                                mobilePhone: contacts[i].phoneNumbers[j].value.replace(/\s+/g, '')
-                            });
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Update $scope.contacts.
-            $scope.$apply();
-
-            // Set a class once a contact is selected.
-            $('#id-contacts .contact').each(function() {
-                $(this).on('click', function(e) {
-                    // Toggles active class.
-                    if ($(this).hasClass('active')) {
-                        $(this).removeClass('active');
-                        $(this).children('span.glyphicon').removeClass('glyphicon-ok')
-                    } else {
-                        $(this).addClass('active');
-                        $(this).children('span.glyphicon').addClass('glyphicon-ok')
-                    }
-                });
-            });
-
-            // All done.
-            appStatus.completed('contacts');
-        };
-
-        function onError(contactError) {
-            notifier.show($scope.lang.error, $scope.lang.errornocontacts, 'error');
-            console.log('Error getting contacts: ' + contactError);
-
-            appStatus.completed('contacts');
-        };
-
-        var filter = [
-            navigator.contacts.fieldType.displayName,
-            navigator.contacts.fieldType.name
-        ];
-        // TODO Try to filter by something (not null...) to avoid the big big iteration.
-        var options = new ContactFindOptions();
-        options.filter = "";
-        options.multiple = true;
-        options.desiredFields = [
-            navigator.contacts.fieldType.displayName,
-            navigator.contacts.fieldType.name,
-            navigator.contacts.fieldType.phoneNumbers
-        ];
-        navigator.contacts.find(filter, onSuccess, onError, options);
-
-    }, false);
-
-    $scope.noSms = function() {
+    $scope.noShare = function() {
 
         newLogRequest('click', 'dishes-share-continue');
 
@@ -92,36 +23,7 @@ angular.module('eat-this-one')
         redirecter.redirect('index.html');
     };
 
-    $scope.sendSms = function() {
-
-        appStatus.waiting('selectedContacts');
-
-        // Get all selected contacts.
-        var phonesArray = [];
-        $('#id-contacts .contact.active').each(function() {
-            console.log('contact! ' + this.id);
-            phonesArray.push(this.id);
-        });
-
-        // The user should explicitly press 'Skip'.
-        if (phonesArray.length === 0) {
-            notifier.show($scope.lang.nocontacts, $scope.lang.nocontactsinfo, 'warning');
-            appStatus.completed('selectedContacts');
-            return false;
-        }
-
-        // The receiver already knows who is sending the message.
-        var msg = $scope.lang.imcooking + ' ' + urlParser.getParam('dishname') + '. ' +
-             $scope.lang.joindetailsbook + '. ' + $scope.lang.groupcode +
-             ': "' + urlParser.getParam('locationname') + '". ' + eatConfig.downloadAppUrl;
-
-        appStatus.completed('selectedContacts');
-
-        var phonesStr = phonesArray.join(',');
-
-        newLogRequest('click', 'share-contacts', phonesStr);
-
-        // Open sms app.
-        window.plugins.socialsharing.shareViaSMS(msg, phonesStr);
+    $scope.share = function() {
+        shareManager.process($scope);
     }
 }]);
