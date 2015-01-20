9df574859653617cfa2b94631075ad1d7f3d8f0b
David Monllao <david.monllao@gmail.com>
2015-01-20 19:21:13 +0800

    [FRONTEND] We don't care about security in web

--- public/javascripts/web-services/pushManager.js
+++ public/javascripts/web-services/pushManager.js
@@ -9,6 +9,8 @@ angular.module('eat-this-one')
             }
 
             // Just a fake per-session reg id for testing purposes.
+            // We don't care if it is stored in localStorage as we don't
+            // care about security in web.
             var randomNumber = Math.random() + new Date().getTime();
             localStorage.setItem('gcmRegId', randomNumber);
         }
