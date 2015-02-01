0171cad13693e424d089026153783a0b79a91c2d
David Monllao <david.monllao@gmail.com>
2015-02-01 14:20:05 +0800

    [FRONTEND] Moving out from angular bootstrap

--- public/javascripts/directives/radio.js
+++ public/javascripts/directives/radio.js
@@ -1,10 +1,34 @@
-angular.module('eat-this-one').directive('eatRadio', function() {
+angular.module('eat-this-one').directive('eatRadio', ['$timeout', function($timeout) {
 
     return {
         restrict: 'E',
         scope: {
             element: '='
         },
+        link : function(scope, element, attrs) {
+
+            // Set the styles for the preset value.
+            $timeout(function() {
+                var defaultOptionId = 'id-option-' + scope.element.name + '-' + scope.element.value;
+                console.log(angular.element('#' + defaultOptionId));
+                angular.element('#' + defaultOptionId).addClass('md-primary');
+                angular.element('#' + defaultOptionId).addClass('md-raised');
+            });
+
+            scope.setValue = function(value) {
+
+                // Set the value.
+                scope.element.value = value;
+
+                // Set style.
+                for (var i = 0; i < scope.element.options.length; i++) {
+                    angular.element('#id-option-' + scope.element.name + '-' + scope.element.options[i].value).removeClass('md-primary');
+                    angular.element('#id-option-' + scope.element.name + '-' + scope.element.options[i].value).removeClass('md-raised');
+                }
+                angular.element('#id-option-' + scope.element.name + '-' + value).addClass('md-primary');
+                angular.element('#id-option-' + scope.element.name + '-' + value).addClass('md-raised');
+            };
+        },
         templateUrl: 'templates/radio.html'
     }
-});
+}]);
