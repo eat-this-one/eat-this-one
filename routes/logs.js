36b8970cefcceb2d649cd96c1e30fbbf9f4868d0
David Monllao <david.monllao@gmail.com>
2015-02-26 07:32:33 +0800

    [BACKEND] Adding EatLogs + standarize EatUsers attrs

--- routes/logs.js
+++ routes/logs.js
@@ -3,81 +3,12 @@ var mongoose = require('mongoose');
 
 var router = express.Router();
 
-var TokenModel = require('../models/token.js').model;
-var LogModel = require('../models/log.js').model;
+var EatLogs = require('../lib/EatLogs.js');
+var Eat = require('../lib/Eat.js');
 
 router.post('/', function(req, res) {
-
-    if (req.param('what') === null) {
-        res.statusCode = 400;
-        res.send("Missing what param, can not add log");
-        return;
-    }
-
-    if (req.param('where') === null) {
-        res.statusCode = 400;
-        res.send("Missing where param, can not add log");
-        return;
-    }
-
-    var log = new LogModel({
-        what : req.param('what'),
-        where : req.param('where'),
-        target : req.param('target')
-    });
-
-    // TODO Please david, learn about callbacks, every
-    // time I see this routers I want to die.
-    if (!req.param('token')) {
-
-        // Guest user.
-        log.userid = 0;
-
-        log.save(function(error) {
-
-            if (error) {
-                res.statusCode = 500;
-                res.send('Error saving log: ' + error);
-                return;
-            }
-
-            res.statusCode = 201;
-            res.send(log);
-            return;
-        });
-
-    } else {
-        // Getting userid from the token if there is a token
-        TokenModel.findOne({token: req.param('token')}, function(error, token) {
-
-            if (error) {
-                res.statusCode = 500;
-                res.send('Error getting token: ' + error);
-                return;
-            }
-
-            if (!token) {
-                res.statusCode = 401;
-                res.send('Wrong credentials');
-                return;
-            }
-
-            log.userid = token.userid;
-
-            log.save(function(error) {
-
-                if (error) {
-                    res.statusCode = 500;
-                    res.send('Error saving log: ' + error);
-                    return;
-                }
-
-                res.statusCode = 201;
-                res.send(log);
-                return;
-            });
-        });
-    }
+    Eat.setReqRes(req, res);
+    EatLogs.addLog();
 });
 
 router.get('/', function(req, res) {
