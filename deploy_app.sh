1d89a535e3da495007a60c090715217008885f14
David Monllao <david.monllao@gmail.com>
2015-03-06 01:43:31 +0800

    [PROJECT] Improve deploy to production script

--- deploy_app.sh
+++ deploy_app.sh
@@ -2,6 +2,33 @@
 
 set -e
 
+if [ -z $1 ]; then
+    echo "
+REMEMBER TO:
+- Point config_frontend.js to the production server
+- Add information in updateManager about the new version cool features
+
+Run deploy_app.sh with a second argument after confirming it :)"
+    exit 0
+fi
+
+# Push last changes to backend server (NO -f HERE!).
+git push dokku master
+
+# Build the app to dist/app compressing JS.
+# TODO Include version bumps.
+grunt build:prod
+
 # The SSH key should be set.
 scp -r dist/app/platforms/android/ant-build/CordovaApp-debug-unaligned.apk \
     root@eat-this-one.com:/home/dokku/download/android.apk
+
+echo "
+-------------------------------------------------------------------------------
+ALL DONE!
+- Production app build ready
+- http://www.eat-this-one.com/android.apk updated to last app release
+- Backend updated to latest master
+
+Now feel free to run ./install_app.sh to set the production app to your device."
+exit 0
