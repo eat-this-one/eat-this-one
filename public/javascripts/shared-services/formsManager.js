b8d81dd31d1d41c0693bd3e9a114d2354edd0f03
David Monllao <david.monllao@gmail.com>
2015-03-17 19:49:26 +0800

    [FRONTEND] LOL!

--- public/javascripts/shared-services/formsManager.js
+++ public/javascripts/shared-services/formsManager.js
@@ -39,7 +39,7 @@ angular.module('eat-this-one').factory('formsManager', function() {
                         case 'text':
                             // Only triggers an error if there is content.
                             if (fields[field].value && fields[field].value.length > 0) {
-                                var textexpression = new RegExp(/^[A-Za-z1-9-_=".',;\s]*$/);
+                                var textexpression = new RegExp(/^[A-Za-z0-9-+_=".',;\s]*$/);
                                 if (!textexpression.test(fields[field].value)) {
                                     errors.push(fields[field].name);
                                     fieldHasErrors = true;
