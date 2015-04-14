c783eb785b38d2c7baa7d5082f5402f9d3baf543
David Monllao <david.monllao@gmail.com>
2015-04-14 21:31:40 +0800

    [FRONTEND] Generic domain

--- install.sh
+++ install.sh
@@ -78,8 +78,7 @@ ${sedcmd} "s#A sample Apache Cordova application that responds to the deviceread
 config.xml
 ${sedcmd} 's#<access.*>#\
     <access origin="'$backend'" />\
-    <access origin="http://s.gravatar.com" />\
-    <access origin="https://s.gravatar.com" />#' config.xml
+    <access origin="http://*.gravatar.com" />#' config.xml
 
 # Only the required platform.
 cordova platform add "$1"
