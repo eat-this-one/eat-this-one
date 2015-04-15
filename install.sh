7eb1697c276d8070ae32c5e72b450b7b9f525bd2
David Monllao <david.monllao@gmail.com>
2015-04-16 07:41:31 +0800

    [FRONTEND] More icons stuff

--- install.sh
+++ install.sh
@@ -56,27 +56,47 @@ ln icons/* dist/app/res
 
 cd dist/app
 
+# I'm tired of dealing with ios resources not reading the
+# filter or picking the wrong icon.
+if [ "$1" == "ios" ]; then
+    ${sedcmd} 's#</widget>#\
+    <icon src="res/icon-ios.png" />\
+    <splash src="res/splash-portrait.png" width="320" height="480"/>\
+    <splash src="res/splash-portrait.png" width="640" height="960"/>\
+    <splash src="res/splash-portrait.png" width="768" height="1024"/>\
+    <splash src="res/splash-portrait.png" width="1536" height="2048"/>\
+    <splash src="res/splash-landscape.png" width="1024" height="768"/>\
+    <splash src="res/splash-landscape.png" width="2048" height="1536"/>\
+    <splash src="res/splash-portrait.png" width="640" height="1136"/>\
+    <splash src="res/splash-portrait.png" width="750" height="1334"/>\
+    <splash src="res/splash-portrait.png" width="1242" height="2208"/>\
+    <splash src="res/splash-landscape.png" width="2208" height="1242"/>\
+    <platform name="ios">\
+        <preference name="EnableViewportScale" value="true"/>\
+    </platform>\
+    </widget>#' config.xml
+else
+    ${sedcmd} 's#</widget>#\
+    <icon src="res/icon-android.png" />\
+    <icon src="res/icon-android.png" density="ldpi" />\
+    <icon src="res/icon-android.png" density="mdpi" />\
+    <icon src="res/icon-android.png" density="hdpi" />\
+    <icon src="res/icon-android.png" density="xhdpi" />\
+    <splash src="res/splash-portrait.png" density="hdpi"/>\
+    <splash src="res/splash-portrait.png" density="ldpi"/>\
+    <splash src="res/splash-portrait.png" density="mdpi"/>\
+    <splash src="res/splash-portrait.png" density="xhdpi"/>\
+    <splash src="res/splash-landscape.png" density="land-hdpi"/>\
+    <splash src="res/splash-landscape.png" density="land-ldpi"/>\
+    <splash src="res/splash-landscape.png" density="land-mdpi"/>\
+    <splash src="res/splash-landscape.png" density="land-xhdpi"/>\
+    </widget>#' config.xml
+
+fi
 # Setting the app config.
-# - Icons & splash
 # - Set minimum supported versions (Android API 14 & IOS 6)
 # - No zoom
 ${sedcmd} 's#</widget>#\
-    <icon src="res/icon-android.png" />\
-    <platform name="ios">\
-        <icon src="res/icon-ios.png" />\
-        <splash src="res/splash-ios.png" />\
-        <preference name="EnableViewportScale" value="true"/>\
-    </platform>\
-    <platform name="android">\
-        <icon src="res/icon-android.png" density="ldpi" />\
-        <icon src="res/icon-android.png" density="mdpi" />\
-        <icon src="res/icon-android.png" density="hdpi" />\
-        <icon src="res/icon-android.png" density="xhdpi" />\
-        <splash src="res/land-android.png" density="land-hdpi"/>\
-        <splash src="res/land-android.png" density="land-ldpi"/>\
-        <splash src="res/land-android.png" density="land-mdpi"/>\
-        <splash src="res/land-android.png" density="land-xhdpi"/>\
-    </platform>\
     <preference name="android-minSdkVersion" value="14" />\
     <preference name="deployment-target" value="6.0" />\
     <preference name="Fullscreen" value="true" />\
@@ -93,12 +113,6 @@ ${sedcmd} 's#<access.*>#\
 
 # Only the required platform.
 cordova platform add "$1"
-
-# Copy manually iOS resources.
-cp res/icon-ios.png platforms/ios/Eat\ this\ one\!/Resources/icons/icon.png
-cp res/splash-ios.png platforms/ios/Eat\ this\ one\!/Resources/splash/Default~iphone.png
-cp res/splash-ios.png platforms/ios/Eat\ this\ one\!/Resources/splash/Default-Portrait~ipad.png
-cp res/land-android.png platforms/ios/Eat\ this\ one\!/Resources/splash/Default-Landscape~ipad.png
 
 # Install all plugins.
 while read -a plugin; do
