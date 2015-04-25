525e042b37354b50c8d998d1ebf0163b9b47a24f
David Monllao <david.monllao@gmail.com>
2015-04-25 23:49:48 +0800

    [PROJECT] Adding config/frontend.js.dist to bin/release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -10,7 +10,7 @@ HEY YOU! BEFORE RUNNING THIS AGAIN REMEMBER TO:
 # Clean the kitchen and commit everthing
 # Copy or increase the version (look the current one in package.json and increment it).
 
-Once done, run 'grunt release:backend:X.Y.Z', where X.Y.Z is the version number.
+Once done, run 'grunt release:X.Y.Z', where X.Y.Z is the version number.
 "
     exit 1
 fi
@@ -48,6 +48,8 @@ if [ -z $version ]; then
     exit 1
 fi
 
+${sedcmd} "s#version *: *'\(.*\)'#version: '$versionCode'#" config/frontend.js.dist
+${sedcmd} "s#version *: *'\(.*\)'#version: '$versionCode'#" config/frontend.js
 ${sedcmd} "s#\"version\" *: *\"\(.*\)\"#\"version\": \"$version\"#" package.json
 ${sedcmd} "s#\"version\" *: *\"\(.*\)\"#\"version\": \"$version\"#" bower.json
 ${sedcmd} "s#version=\"\(.*\)\" xmlns#version=\"$version\" android-versionCode=\"$versionCode\" xmlns#" dist/app/config.xml
@@ -66,6 +68,8 @@ echo "
 -------------------------------------------------------------------------------
 DONE!
 - Backend updated to latest master
-- Version updated in package.json, bower.json and dist/app/config.xml
+- Version updated in package.json, bower.json, config/frontend.js, config/frontend.js.dist and dist/app/config.xml
+- Tag v$version released
+- Public repo master HEAD updated
 "
 exit 0
