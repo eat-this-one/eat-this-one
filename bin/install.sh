d0ee3ceddff3dd5f1e463caf3475f6aefef9be54
David Monllao <david.monllao@gmail.com>
2015-04-29 21:29:35 +0800

    [FRONTEND] Adding alll boring icons

--- bin/install.sh
+++ bin/install.sh
@@ -81,17 +81,31 @@ cd dist/app
 # probably unexisting future.
 if [ "$1" == "ios" ]; then
     ${sedcmd} 's#</widget>#\
-    <icon src="res/icon-ios.png" />\
-    <splash src="res/splash-portrait.png" width="320" height="480"/>\
-    <splash src="res/splash-portrait.png" width="640" height="960"/>\
-    <splash src="res/splash-portrait.png" width="768" height="1024"/>\
-    <splash src="res/splash-portrait.png" width="1536" height="2048"/>\
-    <splash src="res/splash-landscape.png" width="1024" height="768"/>\
-    <splash src="res/splash-landscape.png" width="2048" height="1536"/>\
-    <splash src="res/splash-portrait.png" width="640" height="1136"/>\
-    <splash src="res/splash-portrait.png" width="750" height="1334"/>\
-    <splash src="res/splash-portrait.png" width="1242" height="2208"/>\
-    <splash src="res/splash-landscape.png" width="2208" height="1242"/>\
+    <icon src="res/icon-ios-60@3x.png" width="180" height="180" />\
+    <icon src="res/icon-ios-60.png" width="60" height="60" />\
+    <icon src="res/icon-ios-60@2x.png" width="120" height="120" />\
+    <icon src="res/icon-ios-76.png" width="76" height="76" />\
+    <icon src="res/icon-ios-76@2x.png" width="152" height="152" />\
+    <icon src="res/icon-ios-40.png" width="40" height="40" />\
+    <icon src="res/icon-ios-40@2x.png" width="80" height="80" />\
+    <icon src="res/icon-ios.png" width="57" height="57" />\
+    <icon src="res/icon-ios@2x.png" width="114" height="114" />\
+    <icon src="res/icon-ios-72.png" width="72" height="72" />\
+    <icon src="res/icon-ios-72@2x.png" width="144" height="144" />\
+    <icon src="res/icon-ios-small.png" width="29" height="29" />\
+    <icon src="res/icon-ios-small@2x.png" width="58" height="58" />\
+    <icon src="res/icon-ios-50.png" width="50" height="50" />\
+    <icon src="res/icon-ios-50@2x.png" width="100" height="100" />\
+    <splash src="res/splash-ios.png" width="320" height="480"/>\
+    <splash src="res/splash-ios@2x.png" width="640" height="960"/>\
+    <splash src="res/splash-ios-portrait.png" width="768" height="1024"/>\
+    <splash src="res/splash-ios-portrait@2x.png" width="1536" height="2048"/>\
+    <splash src="res/splash-ios-landscape.png" width="1024" height="768"/>\
+    <splash src="res/splash-ios-landscape@2x.png" width="2048" height="1536"/>\
+    <splash src="res/splash-ios-568h@2x.png" width="640" height="1136"/>\
+    <splash src="res/splash-ios-667h@2x.png" width="750" height="1334"/>\
+    <splash src="res/splash-ios-736h@2x.png" width="1242" height="2208"/>\
+    <splash src="res/splash-ios-landscape-736h@3x.png" width="2208" height="1242"/>\
     <platform name="ios">\
         <preference name="EnableViewportScale" value="true"/>\
     </platform>\
@@ -103,14 +117,18 @@ else
     <icon src="res/icon-android.png" density="mdpi" />\
     <icon src="res/icon-android.png" density="hdpi" />\
     <icon src="res/icon-android.png" density="xhdpi" />\
-    <splash src="res/splash-portrait.png" density="hdpi"/>\
-    <splash src="res/splash-portrait.png" density="ldpi"/>\
-    <splash src="res/splash-portrait.png" density="mdpi"/>\
-    <splash src="res/splash-portrait.png" density="xhdpi"/>\
-    <splash src="res/splash-landscape.png" density="land-hdpi"/>\
-    <splash src="res/splash-landscape.png" density="land-ldpi"/>\
-    <splash src="res/splash-landscape.png" density="land-mdpi"/>\
-    <splash src="res/splash-landscape.png" density="land-xhdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="hdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="ldpi"/>\
+    <splash src="res/splash-android-portrait.png" density="mdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="xhdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="port-hdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="port-ldpi"/>\
+    <splash src="res/splash-android-portrait.png" density="port-mdpi"/>\
+    <splash src="res/splash-android-portrait.png" density="port-xhdpi"/>\
+    <splash src="res/splash-android-landscape.png" density="land-hdpi"/>\
+    <splash src="res/splash-android-landscape.png" density="land-ldpi"/>\
+    <splash src="res/splash-android-landscape.png" density="land-mdpi"/>\
+    <splash src="res/splash-android-landscape.png" density="land-xhdpi"/>\
     </widget>#' config.xml
 
 fi
