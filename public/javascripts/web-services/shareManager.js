776426ac85eeaed6f9264a71a3486da3672e2c14
David Monllao <david.monllao@gmail.com>
2015-05-30 13:01:24 +0800

    [FRONTEND] Deprecating invite through sms
    Some keywords to find this commit in future if needed:
    sms message invite

--- public/javascripts/web-services/shareManager.js
+++ public/javascripts/web-services/shareManager.js
@@ -2,13 +2,8 @@ angular.module('eat-this-one').factory('shareManager', ['eatConfig', 'appStatus'
 
     return {
 
-        init : function($scope) {
-            appStatus.completed('contacts');
-        },
-
-        process : function($scope, msg) {
-            window.location.href = 'mailto:?to=&body=' + msg +
-                '&subject=' + $scope.lang.inviteemailsubject;
+        share : function(msg) {
+            window.location.href = 'mailto:?to=&body=' + msg;
         }
     };
 }]);
